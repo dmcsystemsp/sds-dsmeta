c5aafd6e2a3e97efa3d6b18e91c4170872d6acf8
Daniel Carneiro <daniel@dmcsysteminformatica.com.br>
2022-11-14 23:41:59 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components"
 function App() {
     return (
-    <h1>Olá Mundo!</h1>
- 
+      <>
+       <h1>Olá Mundo!</h1>
+       <NotificationButton/>
+      </>
+   
+
   )
 }
 
